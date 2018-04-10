511033f75414a69b76f3992a9649fce510aea763
Glitch (understood-fall) <none>
2018-04-10 23:56:22 +0000

    🎖📮 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('hi there foo/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the bror's dev tools console
+con');
+sole.log('hi there foo');
